e6ab7d82d0ce15bdf639aad8cbe1e5b218a13d2d
yxjacksonyyds <146959706+yxjacksonyyds@users.noreply.github.com>
2025-08-05 16:45:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,52 @@
-import json
-from get_knowledge import get_bind,get_activity
-from prompt import promptt
-import requests
-from qwen import get_answer
-from transformers import pipeline
-from modelscope import AutoModelForCausalLM, AutoTokenizer
-from accelerate import init_empty_weights  # 新路径
+from get_knowledge import get_id,get_activity,get_bind,get_text,get_drug
+from create_prompt import promptt
+from deepseek import chat
+import re
+from rdkit import Chem
+def extract_smiles(text):
+    pattern = r'<smiles>(.*?)</smiles>'
+    smiles_list = re.findall(pattern, text)
+    if len(smiles_list)>0 :
+        return smiles_list[0]
+    else:
+        return 'not provided'
 
-with open('dataset/filtered_TTD.txt','r') as data:
-    uniprot_list = []
-    chembl_list = []
-    for i in data:
-        list = i.split(' ')
-        uniprot_list.append(list[0])
-        chembl_list.append(list[1])
-# 读取数据集，并加载ID
-
-with open("data_process/process.txt", "w") as file:
-    for uniprotid, chemblid in zip(uniprot_list, chembl_list):
-        if get_bind(uniprotid) and get_activity(chemblid):
-            file.write(f"{uniprotid} || {chemblid} || {get_bind(uniprotid)} || {get_activity(chemblid)}\n")
-        else:
-            continue
-# 读取ID，并构造知识库
-
-listt = []
-with open('data_process/process.txt','r') as file:
-    for i in file:
-        list = i.split(' || ')
-        uniprotid = list[0]
-        chemblid = list[1]
-        bind = list[2]
-        activity = list[3]
-        request = '……'
-        S = promptt(pref_name=pref_name,Ki=activity,binding_site=bind,request=request,chemblid=chemblid)
-        list = S.get_prompt()
-        listt.append(list)
-
-# 根据知识构造prompt
-
-with open('data_process/prompt_list.json', 'w') as json_file:
-    json.dump(listt, json_file, indent=4)
-
-print(f"prompt的json已成功写入到 文件中。")
-
-if __name__ == '__main__':
-    with open('prompt_list.json', 'r') as jsonfile:
-        dict_list = json.load(jsonfile)
-    answer_list = []
-    for i in dict_list:
-        answer = get_answer([i[0],i[1],i[2],i[3]])
-        print(answer)
-        answer_list.append({"answer":answer,"chemblid":i[4]})
-
-    with open('output/qwen_output.txt','w') as file:
-        json.dump(answer_list, file, indent=4)
-
-	pipe = pipeline("text-generation", model="/root/autodl-tmp/drugassist")
-	with open('gpt4_shaixuan_jieguo.txt','r') as file:
-    		Smiles = []
-    		for i in file:
-        	smiles = i.split(' || ')[0]
-        	smiles = get_youhua(smiles,pipe)
-        	Smiles.append(smiles+' || '+i.split(' || ')[1])
-
-	with open('gpt4_shaixuan_jieguo.txt','w') as file:
-    		for i in Smiles:
-        	file.write(i+'\n')
-#优化分子smiles并写回。
+def is_valid_smiles(smiles):
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            return 0
+        # 可选：进行Kekulization验证（检查芳香性等）
+        try:
+            Chem.SanitizeMol(mol)
+            return 1
+        except:
+            return 0
+    except:
+        return 0
+# pref_name_list = ['Epidermal growth factor receptor erbB1','Coagulation factor VII'] 在此键入你的pref_name即可
+pref_name_list = ['Epidermal growth factor receptor erbB1','Coagulation factor VII']
+for i in pref_name_list:
+    pref_name = i
+    uniprotid,chemblid = get_id(pref_name)
+    activity = get_activity(chemblid)
+    drug = get_drug(chemblid)
+    binding_site = get_bind(uniprotid)
+    text = get_text(pref_name)
+    prompt = promptt(pref_name=pref_name, Ki = activity, binding_site=binding_site, request='1',chemblid=chemblid,text=text)
+    prompt_list = prompt.get_prompt()
+    cont = 1
+    print(chemblid)
+    while(cont):
+        answer = chat(prompt_list)
+        smiles = extract_smiles(answer)
+        if is_valid_smiles(smiles):
+            print(smiles)
+            cont = 0
+            break
+    with open('output.txt', 'a') as file:
+        file.write(chemblid)
+        file.write(' ')
+        file.write(smiles)
+        file.write('\n')
+    print('=====================')
